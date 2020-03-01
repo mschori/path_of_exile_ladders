bf04ef1b5a6603cf6f21ef941dbe3da597a2baa8
mschori <michael.schori@gmx.ch>
2020-03-01 08:47:07 +0100

    Updated for new PoE-League "Delirium"

--- app/src/main/java/com/example/m335_poe_ladders/MainActivity.java
+++ app/src/main/java/com/example/m335_poe_ladders/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.m335_poe_ladders;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -21,10 +22,54 @@ public class MainActivity extends AppCompatActivity {
         this.intent = new Intent(MainActivity.this, LadderActivity.class);
 
         // Get views
+        TextView deliriumStandard = (TextView) findViewById(R.id.delirium_standard);
+        ImageView deliriumStandardArrow = (ImageView) findViewById(R.id.delirium_standard_arrow);
+        TextView deliriumHardcore = (TextView) findViewById(R.id.delirium_hardcore);
+        ImageView deliriumHardcoreArrow = (ImageView) findViewById(R.id.delirium_hardcore_arrow);
         TextView metamorphStandard = (TextView) findViewById(R.id.metamorph_standard);
         ImageView metamorphStandardArrow = (ImageView) findViewById(R.id.metamorph_standard_arrow);
         TextView metamorphHardcore = (TextView) findViewById(R.id.metamorph_hardcore);
         ImageView metamorphHardcoreArrow = (ImageView) findViewById(R.id.metamorph_hardcore_arrow);
+
+        // Set a click listener on delirium standard
+        deliriumStandard.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Bind extra to intent and start intent
+                intent.putExtra("league", "delirium_standard");
+                startActivity(intent);
+            }
+        });
+        deliriumStandardArrow.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Bind extra to intent and start intent
+                intent.putExtra("league", "delirium_standard");
+                startActivity(intent);
+            }
+        });
+
+        // Set a click listener on delirium hardcore
+        deliriumHardcore.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Bind extra to intent and start intent
+                intent.putExtra("league", "delirium_hardcore");
+                startActivity(intent);
+            }
+        });
+        deliriumHardcoreArrow.setOnClickListener(new View.OnClickListener() {
+            // The code in this method will be executed when the numbers category is clicked on.
+            @Override
+            public void onClick(View view) {
+                // Bind extra to intent and start intent
+                intent.putExtra("league", "delirium_hardcore");
+                startActivity(intent);
+            }
+        });
 
         // Set a click listener on metamorph standard
         metamorphStandard.setOnClickListener(new View.OnClickListener() {
